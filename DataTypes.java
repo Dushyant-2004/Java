68d472a804ee6164f6d7d199a34d0ce5221067c8
Dushyant-2004 <dushyantvasisht@gmail.com>
2026-08-23 17:00:26 +0530

    Types

---
+++ DataTypes.java
@@ -0,0 +1,31 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        // Integers --> byte, short, int, long
+        // Binary(2), Octal(8), Hexadecimal(16) number system
+
+        // byte b = 5;
+        // byte b = 0b101;
+
+        byte b = 5;
+        short s = 10;
+        int i = 4000;
+        long l = 10000;
+
+        // Real numbers
+        float f = 10.54f;
+        double d = 23.0987;
+
+        // characters
+        char c = 'a'; // 'a' --> integer --> binary --> store
+
+        // boolean
+        boolean bool = false;
+
+        System.out.println("Integer values --> " + b + ", " + s + ", " + i + ", " + l);
+        System.out.println("Floating values --> " + f + ", " + d);
+        System.out.println("Character values --> " + c);
+        System.out.println("Boolean values --> " + bool);
+    }
+}
