bc8b1741d8200487784ef29d3f7b6abddc3eb36d
Dushyant-2004 <dushyantvasisht@gmail.com>
2026-09-15 19:55:47 +0530

    Boring

--- Patterns.java
+++ Patterns.java
@@ -20,5 +20,14 @@ public class Patterns {
             }
             System.out.println();
         }
+
+         System.out.println("Pattern 3:");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
     }
 }
+
